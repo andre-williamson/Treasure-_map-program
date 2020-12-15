61def8eca7159cbcf534bff7f7cb64be27dbf2db
andre williamson <AndreWilliamsonMN@gmail.com>
2020-12-15 13:59:42 +0000

    did it

--- main.py
+++ main.py
@@ -6,15 +6,13 @@ map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 # 🚨 Don't change the code above 👆
-
-#Write your code below this row 👇
-
-
-
+import random
+rows = position[0]
+column = position[1]
 
 
+int_postion = int(position)
 
-#Write your code above this row 👆
+selected = map[ -1]
 
-# 🚨 Don't change the code below 👇
 print(f"{row1}\n{row2}\n{row3}")
